434e5f33e7315a19f57481c4e6f88f09c79e77eb
Felisia17 <felisia.passarelli@icloud.com>
2026-02-05 11:16:51 +0100

    database e migrazioni

---
+++ database/migrations/2026_02_05_092337_create_motorbikes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('motorbikes', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand');
+            $table->string('model');
+            $table->integer('engine_cc');
+            $table->integer('horsepower');
+            $table->year('production_year');
+            $table->text('description');
+            $table->string('body_type');
+            $table->integer('doors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('motorbikes');
+    }
+};
